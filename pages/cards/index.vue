c5fd97cca982e7c17a29dc3f056907d8eb5d1f5c
Rahul Soni <rahulsoni2225@gmail.com>
2018-07-16 23:19:56 +0530

    team cards done

---
+++ pages/cards/index.vue
@@ -0,0 +1,208 @@
+</<template>
+<section>
+ <div class="section">
+  <div class="container">
+    <div class="columns">
+      <div class="column is-half">
+        <div class="card">
+          <div class="header">
+            <div class="avatar">
+              <img src="https://image.ibb.co/fa2YRF/dounia.jpg" alt="">
+            </div>
+          </div>
+          <div class="card-body">
+            <div class="user-meta has-text-centered">
+              <h3 class="username">Helen Miller</h3>
+              <h5 class="position">Accountant</h5>
+            </div>
+            <div class="user-bio has-text-centered">
+              <p>Helen Miller is an accountant at the Acme Inc comany. She works very hard.</p>
+            </div>
+            <div class="action has-text-centered">
+              <a href="#" class="button is-small">View profile</a>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+        <div class="card">
+          <div class="header">
+            <div class="avatar">
+              <img src="https://image.ibb.co/fa2YRF/dounia.jpg" alt="">
+            </div>
+          </div>
+          <div class="card-body">
+            <div class="user-meta has-text-centered">
+              <h3 class="username">Helen Miller</h3>
+              <h5 class="position">Accountant</h5>
+            </div>
+            <div class="user-bio has-text-centered">
+              <p>Helen Miller is an accountant at the Acme Inc comany. She works very hard.</p>
+            </div>
+            <div class="action has-text-centered">
+              <a href="#" class="button is-small">View profile</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="columns">
+
+      <div class="column">
+        <div class="card">
+          <div class="header">
+            <div class="avatar">
+              <img src="https://image.ibb.co/fa2YRF/dounia.jpg" alt="">
+            </div>
+          </div>
+          <div class="card-body">
+            <div class="user-meta has-text-centered">
+              <h3 class="username">Helen Miller</h3>
+              <h5 class="position">Accountant</h5>
+            </div>
+            <div class="user-bio has-text-centered">
+              <p>Helen Miller is an accountant at the Acme Inc comany. She works very hard.</p>
+            </div>
+            <div class="action has-text-centered">
+              <a href="#" class="button is-small">View profile</a>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+         <div class="card">
+          <div class="header">
+            <div class="avatar">
+              <img src="https://image.ibb.co/fa2YRF/dounia.jpg" alt="">
+            </div>
+          </div>
+          <div class="card-body">
+            <div class="user-meta has-text-centered">
+              <h3 class="username">Helen Miller</h3>
+              <h5 class="position">Accountant</h5>
+            </div>
+            <div class="user-bio has-text-centered">
+              <p>Helen Miller is an accountant at the Acme Inc comany. She works very hard.</p>
+            </div>
+            <div class="action has-text-centered">
+              <a href="#" class="button is-small">View profile</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>
+  <a href="https://cssninja.io" class="link-button">
+    <img src="https://image.ibb.co/dR3ZRm/cssninja_icon_white.png">
+  </a>
+</div>
+</section>
+</template>
+>
+
+<style lang="scss" scoped>
+body {
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+
+.columns {
+  margin-top: 0;
+}
+
+.section {
+  background-color: #ededed;
+  min-height: 100vh;
+  position: relative;
+}
+.section .card {
+  border-radius: 6px;
+  overflow: hidden;
+  max-width: 300px;
+  max-height: 380px;
+  margin: 0 auto;
+}
+.section .card .header {
+  height: 120px;
+  background: #7F00FF;
+}
+.section .card .header .avatar {
+  width: 80px;
+  height: 100%;
+  position: relative;
+  margin: 0 auto;
+}
+.section .card .header .avatar img {
+  display: block;
+  border-radius: 50%;
+  position: absolute;
+  bottom: -42px;
+  border: 4px solid white;
+}
+.section .card .card-body {
+  padding: 30px;
+}
+.section .card .card-body .user-meta {
+  padding-top: 20px;
+}
+.section .card .card-body .user-meta .username {
+  font-size: 18px;
+  font-weight: 600;
+}
+.section .card .card-body .user-meta .position {
+  font-size: 90%;
+  color: #7F00FF;
+}
+.section .card .user-bio {
+  padding-top: 8px;
+  font-size: 92%;
+  color: #999;
+}
+.section .card .action {
+  padding-top: 20px;
+}
+.section .card .action .button {
+  padding: 16px 20px 16px 20px;
+  background: #7F00FF;
+  border-color: #7F00FF;
+  color: white;
+  border-radius: 100px;
+  transition: opacity .3s;
+}
+.section .card .action .button:hover {
+  opacity: 0.7;
+}
+
+.link-button {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background: #7F00FF;
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all .4s;
+}
+.link-button img {
+  width: 32px;
+  height: 32px;
+  display: block;
+}
+.link-button:hover {
+  transform: scale(1.1) rotate(180deg);
+  background: #00D1B2;
+}
+.bookmark article {
+  margin: 1rem 0;
+}
+.bookmark h2 {
+  color: #363636;
+}
+
+
+</style>
+>
---
+++ pages/team/index.vue
@@ -0,0 +1,338 @@
+<<template>
+<section class="hero is-fullheight is-primary is-bold">
+  <div class="hero-body justify-center">
+    <div class="columns">
+         <div class="section">
+         <div class="box bookmark box-0">
+        <div class="card">
+                <div class="header">
+                    <div class="media">
+                        <div class="media-left">
+                            <figure class="image is-48x48">
+                                <img src="https://source.unsplash.com/random/96x96" alt="Placeholder image">
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <p class="title is-4">John Smith</p>
+                            <p class="subtitle is-6">@johnsmith</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-image">
+                    <figure class="image is-4by3">
+                        <img src="https://source.unsplash.com/random/1280x960" alt="Placeholder image">
+                    </figure>
+                </div>
+                <div class="card-content">
+                    <div class="level is-mobile">
+                        <div class="level-left">
+                            <div class="level-item has-text-centered">
+                                <a href="">
+                                    <i class="material-icons">favorite_border</i>
+                                </a>
+                            </div>
+                            <div class="level-item has-text-centered">
+                                <div>
+                                    <a href="">
+                                        <i class="material-icons">chat_bubble_outline</i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="content">
+                        <p>
+                            <strong>32 Likes</strong>
+                        </p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                        <a>@bulmaio</a>.
+                        <a href="#">#css</a>
+                        <a href="#">#responsive</a>
+                        <br>
+                        <time datetime="2018-1-1">11:09 PM - 1 Jan 2018</time>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div class="columns is-mobile">
+                        <div class="column is-11">
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input is-medium" type="text" placeholder="Add a comment . . .">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="column has-text-centered">
+                            <button class="button">
+                                <i class="material-icons">more_horiz</i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <div class="columns">
+            <div class="section">
+         <div class="box bookmark box-0">
+            <div class="card">
+                <div class="header">
+                    <div class="media">
+                        <div class="media-left">
+                            <figure class="image is-48x48">
+                                <img src="https://source.unsplash.com/random/96x96" alt="Placeholder image">
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <p class="title is-4">John Smith</p>
+                            <p class="subtitle is-6">@johnsmith</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-image">
+                    <figure class="image is-4by3">
+                        <img src="https://source.unsplash.com/random/1280x960" alt="Placeholder image">
+                    </figure>
+                </div>
+                <div class="card-content">
+                    <div class="level is-mobile">
+                        <div class="level-left">
+                            <div class="level-item has-text-centered">
+                                <a href="">
+                                    <i class="material-icons">favorite_border</i>
+                                </a>
+                            </div>
+                            <div class="level-item has-text-centered">
+                                <div>
+                                    <a href="">
+                                        <i class="material-icons">chat_bubble_outline</i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="content">
+                        <p>
+                            <strong>32 Likes</strong>
+                        </p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                        <a>@bulmaio</a>.
+                        <a href="#">#css</a>
+                        <a href="#">#responsive</a>
+                        <br>
+                        <time datetime="2018-1-1">11:09 PM - 1 Jan 2018</time>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div class="columns is-mobile">
+                        <div class="column is-11">
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input is-medium" type="text" placeholder="Add a comment . . .">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="column has-text-centered">
+                            <button class="button">
+                                <i class="material-icons">more_horiz</i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+             <div class="columns">
+            <div class="section">
+         <div class="box bookmark box-0">
+            
+            <div class="card">
+                <div class="header">
+                    <div class="media">
+                        <div class="media-left">
+                            <figure class="image is-48x48">
+                                <img src="https://source.unsplash.com/random/96x96" alt="Placeholder image">
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <p class="title is-4">John Smith</p>
+                            <p class="subtitle is-6">@johnsmith</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-image">
+                    <figure class="image is-4by3">
+                        <img src="https://source.unsplash.com/random/1280x960" alt="Placeholder image">
+                    </figure>
+                </div>
+                <div class="card-content">
+                    <div class="level is-mobile">
+                        <div class="level-left">
+                            <div class="level-item has-text-centered">
+                                <a href="">
+                                    <i class="material-icons">favorite_border</i>
+                                </a>
+                            </div>
+                            <div class="level-item has-text-centered">
+                                <div>
+                                    <a href="">
+                                        <i class="material-icons">chat_bubble_outline</i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="content">
+                        <p>
+                            <strong>32 Likes</strong>
+                        </p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                        <a>@bulmaio</a>.
+                        <a href="#">#css</a>
+                        <a href="#">#responsive</a>
+                        <br>
+                        <time datetime="2018-1-1">12:02 PM - 1 May 2018</time>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div class="columns is-mobile">
+                        <div class="column is-11">
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input is-medium" type="text" placeholder="Add a comment . . .">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="column has-text-centered">
+                            <button class="button">
+                                <i class="material-icons">more_horiz</i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+</section>
+</template>>
+
+<style scoped>
+.body-columns {
+    margin-top: 10vh;
+}
+.card {
+    margin-top: 5rem;
+}
+.card .header {
+    padding: 5px 10px;
+}
+.card-footer .columns {
+    width: 100%;
+}
+.card-footer .columns input {
+    border: none;
+    border-radius: 0;
+    box-shadow: none;
+}
+.card-footer .columns .column:last-child {
+    display: flex;
+    align-items: center;
+}
+.card-footer .columns .column:last-child button {
+    border: none;
+}
+.footer {
+    margin-top: 10vh;
+    padding: 2rem 1.5rem;
+}
+
+/* @media screen and (max-width: 786px){
+    .navbar {
+        justify-content: space-between;
+    }
+} */
+.image-circle img {
+  border-radius: 50%;
+}
+
+.bookmark article {
+  margin: 1rem 0;
+}
+.bookmark h2 {
+  color: #363636;
+}
+
+.button, .link {
+  transition: 400ms ease;
+}
+
+#home-btn {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  z-index: 999;
+  color: whitesmoke;
+  transition: 400ms ease;
+}
+
+.footer {
+  box-shadow: inset 2px 2px 4px #999;
+}
+
+.justify-center {
+  justify-content: center;
+}
+
+.list-clean {
+  list-style: none;
+}
+.list-clean .icon {
+  margin-right: 12px;
+}
+
+.tile.is-child.box {
+  background-color: whitesmoke;
+  position: relative;
+  transition: 400ms;
+}
+.tile.is-child.box:hover {
+  box-shadow: 1px 2px 3px #00d1b2, 0 0 0 1px #009e86;
+}
+.tile.is-child.box .link {
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
+}
+.tile.is-child.box .image-link {
+  background: #fff;
+}
+.tile.is-child.box .image-link img {
+  opacity: 1;
+  transition: 300ms ease;
+}
+.tile.is-child.box .image-link img:hover {
+  opacity: 0.64;
+}
+
+.tile.is-child.notification {
+  transition: 400ms;
+}
+.tile.is-child.notification:hover {
+  box-shadow: 1px 2px 3px #00d1b2, 0 0 0 1px #009e86;
+}
+.tile.is-child.notification .link {
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
+}
+
+</style>
+>
+ 
