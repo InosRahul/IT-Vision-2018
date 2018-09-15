6b043bdab04b3a0bd959defd4d644dfad68d3d7c
Rahul Soni <rahulsoni2225@gmail.com>
2018-09-15 22:35:37 +0530

    changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,9 +70,7 @@ module.exports = {
       appId: '60ca73b3-a80c-4028-a782-df0034adf02e',
       allowLocalhostAsSecureOrigin: true,
       persistNotification: true,
-      welcomeNotification: {
-          disable: false
-      }
+      welcomeNotification: true,
     }
   },
   
