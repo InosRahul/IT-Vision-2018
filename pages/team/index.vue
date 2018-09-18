437d97f6e6087fb83575d00e00c6dbebc67d3e1d
Rahul Soni <rahulsoni2225@gmail.com>
2018-09-18 18:50:14 +0530

    chnages

--- pages/team/index.vue
+++ pages/team/index.vue
@@ -53,7 +53,7 @@
 <div class="card">
 <div class="header">
 <div class="circle">
-  <img src="~assets/dhwani.jpg" alt="" />
+  <img src="~assets/dhwanis.jpg" alt="" />
   <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" style="enable-background:new -580 439 577.9 194;"
      xml:space="preserve">  
     <circle cx="50" cy="50" r="40" />
