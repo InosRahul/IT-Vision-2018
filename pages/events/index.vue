e5388772ca9afeb44443974a2c2971e2916f13f7
Rahul Soni <rahulsoni2225@gmail.com>
2018-09-19 23:08:24 +0530

    changes

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,115 +1,48 @@
 <<template>
 <section>
-    <section class="cmyk">
-  <span><nuxt-link class="link1" to="/Technical">Technical</nuxt-link></span>
-</section>
-
-<section class="skewed">
-  
-  <span><nuxt-link class="link2" to="/Non-Technical">Non-Tech</nuxt-link></span>
-  
-</section>
-
-
-<section class="thin-shade">
-  
-  <span><nuxt-link class="link3" to="/Workshop" active-class="nuxt-link-active" >Workshops</nuxt-link></span>
-
-</section>
+    <h1 class='elegantshadow'><nuxt-link class="link1" to="/Technical">Technical</nuxt-link></h1>
+    
+<h1 class='deepshadow'><nuxt-link class="link2" to="/Non-Technical">Non-Technical</nuxt-link></h1>
+<h1 class='retroshadow'><nuxt-link class="link3" to="/Workshop">Workshops</nuxt-link></h1>
 </section>
 </template>>
 
-
->
-
-<style lang="scss" scoped>
-a.nuxt-link-active{
-  color: white;
-}
-
-section {
-  width: 100%;
+<style scoped>
+h1 {
+  font-family: "Avant Garde", Avantgarde, "Century Gothic", CenturyGothic, "AppleGothic", sans-serif;
+  font-size: 92px;
+  padding: 80px 50px;
   text-align: center;
-  padding: 10px 0;
-  font-size: 80px;
- 
+  text-transform: uppercase;
+  text-rendering: optimizeLegibility;
 }
-
-span {
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-}
-
 .link1{
-  color: black;
+  color:#131313;
 }
 .link2{
-  color: black;
+    color:#e0dfdc;
 }
 .link3{
-  color: #F7EB98;
+    color:#2c2c2c;
 }
-// ----------------------------------------------------------------------
-
-.cmyk {
-  background: url(//stephy.mccdgm.net/images/patterns/lightpaperfibers.png);
-
+h1.elegantshadow {
+  color: #131313;
+  background-color: #e7e5e4;
+  letter-spacing: 0.15em;
+  text-shadow: 1px -1px 0 #767676, -1px 2px 1px #737272, -2px 4px 1px #767474, -3px 6px 1px #787777, -4px 8px 1px #7b7a7a, -5px 10px 1px #7f7d7d, -6px 12px 1px #828181, -7px 14px 1px #868585, -8px 16px 1px #8b8a89, -9px 18px 1px #8f8e8d, -10px 20px 1px #949392, -11px 22px 1px #999897, -12px 24px 1px #9e9c9c, -13px 26px 1px #a3a1a1, -14px 28px 1px #a8a6a6, -15px 30px 1px #adabab, -16px 32px 1px #b2b1b0, -17px 34px 1px #b7b6b5, -18px 36px 1px #bcbbba, -19px 38px 1px #c1bfbf, -20px 40px 1px #c6c4c4, -21px 42px 1px #cbc9c8, -22px 44px 1px #cfcdcd, -23px 46px 1px #d4d2d1, -24px 48px 1px #d8d6d5, -25px 50px 1px #dbdad9, -26px 52px 1px #dfdddc, -27px 54px 1px #e2e0df, -28px 56px 1px #e4e3e2;
 }
-
-.cmyk span {
-  color: 000;
-  padding-bottom: 30px;
-  font-family: 'Fredoka One', sans-serif;
-  text-shadow:  0.25rem -0.25rem rgba(magenta, 0.75),
-               -0.25rem -0.25rem rgba(cyan, 0.75),
-                0        0.5rem  rgba(yellow, 0.75);
-  
-
+h1.deepshadow {
+  color: #e0dfdc;
+  background-color: #333;
+  letter-spacing: 0.1em;
+  text-shadow: 0 -1px 0 #fff, 0 1px 0 #2e2e2e, 0 2px 0 #2c2c2c, 0 3px 0 #2a2a2a, 0 4px 0 #282828, 0 5px 0 #262626, 0 6px 0 #242424, 0 7px 0 #222, 0 8px 0 #202020, 0 9px 0 #1e1e1e, 0 10px 0 #1c1c1c, 0 11px 0 #1a1a1a, 0 12px 0 #181818, 0 13px 0 #161616, 0 14px 0 #141414, 0 15px 0 #121212, 0 22px 30px rgba(0, 0, 0, 0.9);
 }
-
-// ----------------------------------------------------------------------
-
-.skewed {
-  background: url(//stephy.mccdgm.net/images/patterns/dark_wood.png) #2b2b2b;  
-  
+h1.retroshadow {
+  color: #2c2c2c;
+  background-color: #d5d5d5;
+  letter-spacing: 0.05em;
+  text-shadow: 4px 4px 0px #d5d5d5, 7px 7px 0px rgba(0, 0, 0, 0.2);
 }
-
-.skewed span {
-  font-family: Oswald, sans-serif;
-  font-size: 55px;
-  text-transform: uppercase;
-  padding: 30 15px; /* to extend the border past the text */
-  
-  margin: 40px 10px;
-  background: #FFDF00;
-  box-shadow: 0 0 0px 10px #FFDF00;
-  border: 10px solid #fff;
-  transform: rotate(-20.5deg) skew(-20.5deg);
-}
-
-// ----------------------------------------------------------------------
-
-.thin-shade  {
-  background: #89DFB8;
-  font: 60px 'Montserrat', sans-serif;
-  text-transform: uppercase;
-  letter-spacing: 0;
-  padding-top: 60x;
-  margin-top: 70px;
-  color: #F7EB98;
-  
-  text-shadow: -4px 4px 0 #89DFB8,
-				      -6px 6px 0 rgba(255,255,255,0.5);
-}
-.thin-shade span{
-  padding-top: 15px;
-  padding-bottom: 15px;
-  
-  
-}
-// ----------------------------------------------------------------------
 
 </style>
 >
-
