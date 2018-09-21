02f6be65165d0290f3ea9059ac0c79b29176def3
Rahul Soni <rahulsoni2225@gmail.com>
2018-09-21 18:56:55 +0530

    tarang changes

--- pages/team/index.vue
+++ pages/team/index.vue
@@ -212,7 +212,7 @@
   <div class="card">
   <div class="header">
   <div class="circle">
-    <img src="~assets/tarang.jpg" alt="" />
+    <img src="~assets/tarang.png" alt="" />
     <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" style="enable-background:new -580 439 577.9 194;"
       xml:space="preserve">  
       <circle cx="50" cy="50" r="40" />
